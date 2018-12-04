34b7259e624704ecab70aef27ae8202d2807be9b
Chris Hartman <chris.m.hartman@gmail.com>
2018-12-04 11:44:05 -0900

    Solves day 1 star 1

--- AdventOfCode2018/main.cpp
+++ AdventOfCode2018/main.cpp
@@ -7,9 +7,29 @@
 //
 
 #include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+#include <algorithm>
+#include <iterator>
+#include <vector>
+using std::vector;
+#include <range/v3/all.hpp>
+namespace r=ranges;
+namespace rv=r::view;
+#include <fstream>
+using std::ifstream;
+#include <string>
+using std::string;
 
-int main(int argc, const char * argv[]) {
-   // insert code here...
-   std::cout << "Hello, World!\n";
+int main() {
+   ifstream fin("/Users/hartman/Documents/Xcode projects/AdventOfCode2018/day1");
+   cout << "Reading input." << endl;
+   std::istream_iterator<string> cinBegin{fin};
+   std::istream_iterator<string> cinEnd;
+   vector<string> v;
+   std::copy(cinBegin,cinEnd,std::back_inserter(v));
+   auto numbers = rv::all(v) | rv::transform([](auto x){return std::stoi(x);});
+   cout << r::accumulate(numbers,0) << endl;
    return 0;
 }
