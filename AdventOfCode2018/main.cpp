9f5762b5627d732f0a2f96c27098f0c7c13859a0
Chris Hartman <chris.m.hartman@gmail.com>
2018-12-04 15:11:07 -0900

    Solves Day 1 star 2

--- AdventOfCode2018/main.cpp
+++ AdventOfCode2018/main.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <iterator>
 #include <string>
+#include <unordered_set>
 #include <vector>
 using std::cout;
 using std::endl;
@@ -21,6 +22,7 @@ namespace rv=r::view;
 
 //*********
 //https://blog.tartanllama.xyz/passing-overload-sets/
+//https://www.youtube.com/watch?v=L_QKlAx31Pw
 #define FWD(...) std::forward<decltype(__VA_ARGS__)>(__VA_ARGS__)
 
 #define LIFT(X) [](auto &&... args) \
@@ -31,11 +33,43 @@ return X(FWD(args)...); \
 }
 //*********
 
-int main() {
-   std::ifstream fin("/Users/hartman/Documents/Xcode projects/AdventOfCode2018/day1");
-   cout << "Reading input." << endl;
+const string DIRECTORY = "/Users/hartman/Documents/Xcode projects/AdventOfCode2018/";
+
+template <typename Rng,CONCEPT_REQUIRES_(!r::BoundedRange<Rng>())>
+auto back(Rng r) -> r::value_type_t<r::iterator_t<Rng>> {
+   r::value_type_t<r::iterator_t<Rng>> ret{};
+   for(auto && v:r)
+      ret=std::move(v);
+   return ret;
+}
+
+void day1star1() {
+   std::ifstream fin(DIRECTORY+"day1");
+   auto numbers = r::getlines(fin) | rv::transform(LIFT(std::stoi)) | rv::partial_sum();
+   cout << "day1 star1: " << back(numbers) << endl;
+}
+
+void day1star2() {
+   std::ifstream fin(DIRECTORY+"day1");
    
-   auto numbers = r::getlines(fin) | rv::transform(LIFT(std::stoi));
-   cout << r::accumulate(numbers,0) << endl;
+   std::vector<int> numbers;
+   ranges::copy(r::getlines(fin) | rv::transform(LIFT(std::stoi)), ranges::back_inserter(numbers));
+   
+   std::unordered_set<int> seen;
+   auto notSeenBefore = [&seen](int i) {
+      if(seen.count(i)==0) {
+         seen.insert(i);
+         return true;
+      }
+      return false;
+   };
+   
+   auto repeats = rv::all(numbers) | rv::cycle | rv::partial_sum() | rv::remove_if(notSeenBefore);
+   cout << "day1 star2: " << *repeats.begin() << endl;
+}
+
+int main() {
+   day1star1();
+   day1star2();
    return 0;
 }
