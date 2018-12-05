b2701f4f567bcdf7a826d9c95bc49be25d99129e
Chris Hartman <chris.m.hartman@gmail.com>
2018-12-05 07:09:16 -0900

    Solve day 2 star 2
    Also clean up a bunch of mess.

--- AdventOfCode2018/main.cpp
+++ AdventOfCode2018/main.cpp
@@ -10,29 +10,18 @@
 #include <iostream>
 #include <iterator>
 #include <string>
+#include <tuple>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
 using std::cout;
 using std::endl;
 using std::string;
+using std::get;
 
 #include <range/v3/all.hpp>
 namespace r=ranges;
 namespace rv=r::view;
-
-//*********
-//https://blog.tartanllama.xyz/passing-overload-sets/
-//https://www.youtube.com/watch?v=L_QKlAx31Pw
-#define FWD(...) std::forward<decltype(__VA_ARGS__)>(__VA_ARGS__)
-
-#define LIFT(X) [](auto &&... args) \
-noexcept(noexcept(X(FWD(args)...)))  \
--> decltype(X(FWD(args)...)) \
-{  \
-return X(FWD(args)...); \
-}
-//*********
 
 const string DIRECTORY = "/Users/hartman/Documents/Xcode projects/AdventOfCode2018/";
 
@@ -41,71 +30,83 @@ auto back(Rng &&r) -> r::value_type_t<r::iterator_t<Rng>> {
    r::value_type_t<r::iterator_t<Rng>> ret{};
    for(auto && v:r)
       ret=std::move(v);
-   return ret;
+      return ret;
 }
 
 void day1star1() {
    std::ifstream fin(DIRECTORY+"day1");
-   auto numbers = r::getlines(fin) | rv::transform(LIFT(std::stoi)) | rv::partial_sum;
+   auto numbers = r::istream_range<int>(fin) | rv::partial_sum;
    cout << "Day 1 star 1: " << back(numbers) << endl;
-}
-
-template <typename Rng,CONCEPT_REQUIRES_(r::Range<Rng>())>
-auto repeatedValuesHelper(Rng &&seen, Rng &&input)
-     -> r::any_view<r::value_type_t<r::iterator_t<Rng>>> {
-   if(input.begin() == input.end())
-      return input;
-   auto &&i = *input.begin();
-   if(r::count(seen,i)>0)
-      return rv::concat(rv::single(i),repeatedValuesHelper(r::any_view<int>(seen),r::any_view<int>(rv::drop(input,1))));
-   return repeatedValuesHelper(r::any_view<int>(rv::concat(seen,rv::single(i))),r::any_view<int>(rv::drop(input,1)));
-}
-
-template <typename Rng,CONCEPT_REQUIRES_(r::Range<Rng>())>
-auto repeatedValues(Rng input) {
-   return repeatedValuesHelper( r::any_view<int>(rv::empty<int>()), r::any_view<int>(input));
 }
 
 void day1star2() {
    std::ifstream fin(DIRECTORY+"day1");
    
    auto numbers = r::istream_range<int>(fin) | r::to_vector;
-
+   
    std::unordered_set<int> seen;
    auto repeats = rv::all(numbers)
-                     | rv::cycle
-                     | rv::partial_sum
-                     | rv::remove_if([&seen](int i) {return seen.insert(i).second;}); //insert returns true if it's new
-   
-   cout << "Day 1 star 2: " << *repeats.begin() << endl;
+   | rv::cycle
+   | rv::partial_sum
+   | rv::filter([&seen](int i) {return !seen.insert(i).second;}) //insert returns true if it's new
+   | rv::take(1);
+   cout << "Day 1 star 2: " << repeats << endl;
 }
-
-
 
 void day2star1() {
    auto hasRep2Rep3 = [](std::string_view s) {
       std::unordered_map<char,int> times;
       for(auto c:s)
          times[c]++;
-      auto twos = r::count_if(times,[](auto p){return p.second==2;});
-      auto threes = r::count_if(times,[](auto p){return p.second==3;}); //Not very DRY
-//    return std::make_pair(times | r::count(2,&pair<int,int>::second)>0,r::count_if(times,3,second)>0);
-      return std::make_pair(twos>0?1:0,threes>0?1:0);
+      auto twice = r::count_if(times,[](auto p){return p.second==2;});
+      auto thrice = r::count_if(times,[](auto p){return p.second==3;});
+      return std::make_pair(twice>0,thrice>0);
    };
    
    std::ifstream fin(DIRECTORY+"day2");
    auto t = r::accumulate(r::istream_range<string>(fin) | rv::transform(hasRep2Rep3),
-                   std::make_pair(0,0),
-                   [](auto p, auto q) {
-                      return std::make_pair(p.first+q.first,p.second+q.second);
-                   });
+                          std::make_pair(0,0),
+                          [](auto p, auto q) {
+                             if(q.first) ++p.first;
+                             if(q.second) ++p.second;
+                             return p;
+                          });
+   
    cout << "Day 2 star 1: " << t.first * t.second << endl;
 }
 
+void day2star2() {
+   std::ifstream fin(DIRECTORY+"day2");
+   auto ids = r::istream_range<string>(fin) | r::to_vector;
+
+   auto hammingDistance = [](auto &&id1, auto &&id2) {
+      return r::accumulate(rv::zip_with([](auto c1, auto c2){
+         return c1==c2?0:1;
+      },id1,id2),0);
+   };
+   
+   auto matches = [](std::string_view s1, std::string_view s2) {
+      return r::accumulate(rv::zip(s1,s2),std::string{},
+               [](auto s, auto p) {
+                  if(p.first==p.second) s+= p.first; return s;
+               });
+   };
+   
+   auto word = rv::cartesian_product(ids,ids)
+               | rv::filter([&](const auto &p){
+                  const auto &[id1,id2]=p; return hammingDistance(id1,id2)==1;
+                  })
+               | rv::transform([&](const auto &p){
+                  const auto &[id1,id2]=p; return matches(id1,id2);})
+               | rv::take(1);
+   
+   cout << "Day 2 star 2: " << word << "\n";
+}
 
 int main() {
-//   day1star1();
-//   day1star2();
+   day1star1();
+   day1star2();
    day2star1();
+   day2star2();
    return 0;
 }
