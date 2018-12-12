6b1dae63bd3c587c92d4d58b987f43f2661d91b6
Chris Hartman <chris.m.hartman@gmail.com>
2018-12-12 07:10:56 -0900

    Solve day 6 and day 7

--- AdventOfCode2018/main.cpp
+++ AdventOfCode2018/main.cpp
@@ -8,23 +8,37 @@
 
 #include <iostream>
 #include <fstream>
-
-#include <stack>
+#include <queue>
+#include <string>
+#include <set>
+#include <tuple>
+using std::get;
+#include <utility>
 #include <vector>
 using std::cout;
 using std::endl;
+using std::string;
 
-//#include <range/v3/getlines.hpp>
+#include <range/v3/getlines.hpp>
 #include <range/v3/to_container.hpp>
-#include <range/v3/algorithm/min_element.hpp>
-//#include <range/v3/algorithm/sort.hpp>
-#include <range/v3/istream_range.hpp>
+#include <range/v3/algorithm/sort.hpp>
+//#include <range/v3/algorithm/count_if.hpp>
+//#include <range/v3/algorithm/minmax_element.hpp>
+#include <range/v3/algorithm/max_element.hpp>
 //#include <range/v3/view/for_each.hpp>
-#include <range/v3/numeric/accumulate.hpp>
-#include <range/v3/view/take.hpp>
-#include <range/v3/view/remove_if.hpp>
-#include <range/v3/view/transform.hpp>
+//#include <range/v3/numeric/accumulate.hpp>
 //#include <range/v3/view/any_view.hpp>
+//#include <range/v3/view/cartesian_product.hpp>
+//#include <range/v3/view/iota.hpp>
+//#include <range/v3/view/concat.hpp>
+//#include <range/v3/view/drop.hpp>
+//#include <range/v3/view/filter.hpp>
+//#include <range/v3/view/remove_if.hpp>
+//#include <range/v3/view/repeat.hpp>
+//#include <range/v3/view/single.hpp>
+//#include <range/v3/view/take.hpp>
+#include <range/v3/view/transform.hpp>
+//#include <range/v3/view/zip.hpp>
 
 #include "oldDays.hpp"
 #include <sstream>
@@ -32,7 +46,8 @@ using std::endl;
 namespace r=ranges;
 namespace rv=r::view;
 
-void day6stars();
+
+
 
 int main() {
 //   day1star1();
@@ -42,5 +57,7 @@ int main() {
 //   day3stars();
 //   day4stars();
 //   day5stars();
+//   day6stars();
+   day7stars();
    return 0;
 }
