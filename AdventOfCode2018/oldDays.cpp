689a5cd64956d11cc036069b17b7f825d7493227
Chris Hartman <chris.m.hartman@gmail.com>
2018-12-17 14:27:33 -0900

    More Day 17 cleanup

--- AdventOfCode2018/oldDays.cpp
+++ AdventOfCode2018/oldDays.cpp
@@ -1182,3 +1182,102 @@ void day16stars() {
    }
    cout << "Day 16 star 2: " << rr.at(0) << endl;
 }
+
+void day17stars() {
+   struct Vein {
+      int minx, maxx, miny, maxy;
+      static Vein lineToVein (const std::string &line) {
+         std::istringstream sin(line);
+         char single,_;
+         int s1,d1,d2;
+         sin >> single >> _ >> s1 >> _ >> _ >> _ >> d1 >> _ >> _ >> d2;
+         return single=='x' ? Vein{s1,s1,d1,d2} : Vein{d1,d2,s1,s1};
+      }
+   };
+   
+   std::ifstream fin(DIRECTORY+"day17");
+   std::vector<Vein> veins = r::getlines(fin) | rv::transform(Vein::lineToVein);
+   
+   class Scan {
+      struct Position {
+         int x, y;
+         Position below() {return {x,  y+1};}
+         Position left()  {return {x-1,y  };}
+         Position right() {return {x+1,y  };}
+      };
+      
+   public:
+      Scan(const std::vector<Vein>& veins) {
+         // may need to allow water flow outside of leftmost and rightmost veins
+         auto _minX = r::min(veins, std::less<>(), &Vein::minx).minx-1;
+         auto _maxX = r::max(veins, std::less<>(), &Vein::maxx).maxx+1;
+         auto _minY = r::min(veins, std::less<>(), &Vein::miny).miny;
+         auto _maxY = r::max(veins, std::less<>(), &Vein::maxy).maxy;
+         _data = std::vector<std::string>(_maxY-_minY+1,std::string(_maxX-_minX+1,'.'));
+         for(const auto &v:veins)
+            for(int y=v.miny;y<=v.maxy;++y)
+               for(int x=v.minx;x<=v.maxx;++x)
+                  _data[y-_minY][x-_minX] = '#';
+         dropFrom({500-_minX,0});
+         cout << "Day 17 Star 1: " << _damp << ", Star 2: " << _wet << "\n";
+      }
+      
+      char & at(Position p) {
+         return _data[p.y][p.x];
+      }
+      
+      void print() {
+         for(const auto &row : _data)
+            cout << row << "\n";
+      }
+      
+      // the following bool functions return true if water can escape or join other damp areas
+      bool dropFrom(Position p) {
+         if (at(p) == '|')
+            return true;
+         if (at(p) != '.')
+            return false;
+         ++_damp; at(p) = '|';
+         if(p.y == _data.size()-1)
+            return true;
+         if(dropFrom(p.below()))
+            return true;
+         return fillFrom(p);
+      }
+      
+      bool fillFrom(Position p) {
+         auto leftFlood = spread(p,&Position::left);
+         auto rightFlood = spread(p,&Position::right);
+         if (!leftFlood && !rightFlood) {
+            for(auto t=p;at(t)=='|';t=t.left()) {
+               _wet++; at(t)='~';
+            }
+            for(auto t=p.right();at(t)=='|';t=t.right()) {
+               _wet++; at(t)='~';
+            }
+         }
+         return leftFlood || rightFlood;
+      }
+      
+      // dir will be Position::left() or Position::right()
+      bool spread(Position p, Position (Position::*dir)()) {
+         p=(p.*dir)();
+         if (at(p) == '|')
+            return true;
+         if (at(p) != '.')
+            return false;
+         ++_damp;
+         at(p) = '|';
+         if(dropFrom(p.below()))
+            return true;
+         return spread(p,dir);
+      }
+      
+   private:
+      std::vector<std::string> _data;
+      size_t _wet=0;
+      size_t _damp=0;
+   };
+   
+   Scan s{veins};
+}
