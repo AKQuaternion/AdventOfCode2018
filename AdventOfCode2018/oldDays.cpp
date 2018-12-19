f9dafa85b1b42f670e509d39039df49a14dedf48
Chris Hartman <chris.m.hartman@gmail.com>
2018-12-18 19:58:05 -0900

    Move day 15 to old days

--- AdventOfCode2018/oldDays.cpp
+++ AdventOfCode2018/oldDays.cpp
@@ -1381,3 +1381,236 @@ void day18stars()
    Lumberyard l{yard};
    l.run();
 }
+
+namespace day15 {
+   struct Point {//we blur distinction between point and vector
+      int x,y;
+      Point operator+(const Point &rhs) {return {x+rhs.x,y+rhs.y};}
+      bool operator==(const Point &rhs) {return x==rhs.x && y==rhs.y;}
+      std::string to_string() const {
+         return "["+std::to_string(x)+","+std::to_string(y)+"]";
+      }
+      int dist(const Point& p) const {return abs(x-p.x)+abs(y-p.y);}
+   };
+   
+   class Unit {
+      friend class Cave;
+      friend bool operator<(const Unit &lhs, const Unit &rhs) {
+         return std::tie(lhs._loc.y,lhs._loc.x)<std::tie(rhs._loc.y,rhs._loc.x);
+      }
+   public:
+      Unit(int x, int y, char type) :_loc{x,y},_type(type)
+      {}
+      
+      std::string to_string() const {
+         return _hp<1 ? std::string{}
+         : _type + ("@" + _loc.to_string() +"(" + std::to_string(_hp) + ")");
+      }
+      
+   private:
+      char other() const {
+         if(_type == 'E') return 'G';
+         else if (_type == 'G') return 'E';
+         throw 1;
+      }
+      
+      bool isTargetOf(char t, Point loc) const {
+         return _hp>0 && _loc.dist(loc)==1 && t == other();
+      }
+      
+      Point _loc;
+      char _type;
+      int _hp=200;
+   };
+   
+   class Cave {
+   public:
+      Cave(const std::vector<std::string> &cave,int elfAttack)
+      :_cave(cave),_elfAttack(elfAttack) {
+         for(int y=0;y<cave.size();++y)
+            for(int x=0;x<cave[y].size();++x)
+               if (auto s = cave[y][x]; s == 'E' || s == 'G')
+                  _units.emplace_back(x,y,s);
+         //      print();
+      }
+      
+      void print() const {
+         for(const auto &l:_cave)
+            cout << l << endl;
+         
+         for(auto &unit : _units)
+            cout << unit.to_string() << " ";
+         cout << endl;
+      }
+      
+      bool doRound() {
+         std::sort(begin(_units),end(_units));
+         for(Unit &me:_units) {
+            if (me._hp < 1)
+               continue;
+            if(!hasOpponents(me._type))
+               return true;
+            doMovement(me);
+            doAttack(me);
+         }
+         
+         std::sort(begin(_units),end(_units));
+         return false;
+      }
+      bool deadElves() {
+         return r::any_of(_units,[](auto &u){return u._type == 'E' && u._hp < 1;});
+      }
+      int score() {
+         return r::accumulate(_units,0,[](int i, const Unit &u){return i+(u._hp>0?u._hp:0);});
+      }
+   private:
+      
+      void doMovement(Unit& me) {
+         if (hasTargets(me._type,me._loc))
+            return;
+         
+         std::vector<Point> dirs{{0,-1},{-1,0},{1,0},{0,1}};
+         
+         struct Info {
+            Point firstMove,pos;
+            int length;
+            bool operator<(const Info &r) const {
+               return std::tie(length,pos.y,pos.x,firstMove.y,firstMove.x) < std::tie(r.length,r.pos.y,r.pos.x,r.firstMove.y,r.firstMove.x);
+            }
+            bool operator==(const Info &r) const {
+               return std::tie(length,pos.y,pos.x,firstMove.y,firstMove.x) == std::tie(r.length,r.pos.y,r.pos.x,r.firstMove.y,r.firstMove.x);
+            }
+         };
+         
+         std::queue<Info> info;
+         
+         std::set<std::pair<int,int>> visited;
+         for(const auto &d:dirs) {
+            auto pos=me._loc+d;
+            if(at(pos) == '.') {
+               info.push(Info{d,pos,1});
+               visited.insert({pos.x,pos.y});
+            }
+         }
+         
+         //      bool show=false;
+         
+         std::vector<Info> goodPaths;
+         while(!info.empty()) {
+            //         if(show) {
+            //            auto i=info;
+            //            while(!i.empty()) {
+            //               auto x = i.front();
+            //               i.pop();
+            //               cout << x.firstMove.to_string() << " "
+            //               << x.pos.to_string() << "\n";
+            //            }
+            //            cout << "-----------------------------------\n";
+            //         }
+            auto spot = info.front();
+            info.pop();
+            if (hasTargets(me._type,spot.pos)) {
+               goodPaths.push_back(spot);
+               continue;
+            }
+            for(const auto &d:dirs) {
+               Point next = spot.pos + d;
+               if (!visited.count({next.x,next.y}) && at(next) == '.') {
+                  info.push(Info{spot.firstMove,next,spot.length+1});
+                  visited.insert({next.x,next.y});
+               }
+            }
+         }
+         
+         if(goodPaths.empty())
+            return;
+         //      auto first = goodPaths.front();
+         std::sort(goodPaths.begin(),goodPaths.end());
+         //      if (!(first == goodPaths.front())) {
+         //         print();
+         //         cout << "Error moving " << me.to_string() << "\n";
+         //         cout << "***Good:" << first.firstMove.to_string() << " " << first.pos.to_string() << "\n";
+         //         cout << "*** Bad:" << goodPaths.front().firstMove.to_string() << " " << goodPaths.front().pos.to_string() << "\n";
+         //         exit(1);
+         //      }
+         assert(at(me._loc)==me._type);
+         assert(me._hp > 0);
+         at(me._loc) = '.';
+         me._loc = me._loc + goodPaths.front().firstMove;
+         at(me._loc) = me._type;
+      }
+      
+      void doAttack(Unit const &me) {
+         auto targets = _units
+         | rv::transform([](Unit &u){return std::ref(u);})
+         | rv::filter([&me](const Unit &u){return u.isTargetOf(me._type, me._loc);})
+         | r::to_vector;
+         
+         if(!targets.empty()) {
+            r::sort(targets,std::less<>(),&Unit::_hp);
+            auto &target = targets.front().get();
+            assert(me._loc.dist(target._loc)==1);
+            assert(me.other() == target._type);
+            assert(target._hp >0);
+            target._hp -= (me._type == 'E' ? _elfAttack : 3);
+            if (target._hp <= 0)
+               at(target._loc) = '.';
+         }
+      }
+      
+      bool hasTargets(char type, Point loc) {
+         auto targets = _units
+         | rv::filter([type,loc](const Unit &u){
+            return u.isTargetOf(type,loc);});
+         return !targets.empty();
+      }
+      bool hasOpponents(char type) {
+         auto targets = _units
+         | rv::filter([type](const Unit &u){return u._hp>0 && type==u.other();});
+         return !targets.empty();
+      }
+      char & at(Point loc)  {return _cave[loc.y][loc.x];}
+      std::vector<std::string> _cave;
+      std::vector<Unit> _units;
+      int _elfAttack;
+   };
+}
+
+void day15stars() {
+   using namespace day15;
+   Unit u{1,5,'G'};
+   std::ifstream fin(DIRECTORY+"day15");
+   //2 27730
+   //3 36334
+   //4 39514
+   //5 27755
+   //6 28944
+   //7 18740
+   //8 Elf moves right (https://www.reddit.com/r/adventofcode/comments/a7fhax/day_15_can_anyone_give_me_a_hint/)
+   //9 Goblin moves down ^^^
+   std::vector<std::string> cave = r::getlines(fin);
+   
+   Cave cavern(cave,3);
+   int i=0;
+   while(!cavern.doRound()) {
+      ++i;
+      //      cout << "After round " << ++i << "\n";
+      //      cavern.print();
+   }
+   cout << "Day 15 star 1: " << cavern.score() << "*" << i << " = " << cavern.score()*i << "\n";
+   
+   for(int e=4;;++e) {
+      Cave cavern(cave,e);
+      int i=0;
+      while(!cavern.doRound()) {
+         ++i;
+         //      cout << "After round " << ++i << "\n";
+         //      cavern.print();
+      }
+      if (!cavern.deadElves()) {
+         cout << "Day 15 star 2: " << cavern.score() << "*" << i << " = " << cavern.score()*i << "\n";
+         return;
+      }
+   }
+   
+}
